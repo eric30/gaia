9c0745e271c3baa167004e1fcfe48f7238bda3f0
ericchou <echou@mozilla.com>
2012-05-03 14:14:35 +0800

    Better Bluetooth UI

--- apps/settings/js/bluetooth.js
+++ apps/settings/js/bluetooth.js
@@ -3,6 +3,13 @@
 
 'use strict';
 
+var uuidAudioSink     = "0000110B-0000-1000-8000-00805F9B34FB";
+var uuidAudioSource   = "0000110A-0000-1000-8000-00805F9B34FB";
+var uuidAdvAudioDist  = "0000110D-0000-1000-8000-00805F9B34FB";
+var uuidHeadset       = "00001108-0000-1000-8000-00805F9B34FB";
+var uuidHandsfree     = "0000111E-0000-1000-8000-00805F9B34FB";
+var uuidHandsfreeAG   = "0000111F-0000-1000-8000-00805F9B34FB";
+
 window.addEventListener('DOMContentLoaded', function discoverDevices(evt) {
   var gBluetoothAdapter = navigator.mozBluetooth;
   var _ = document.mozL10n.get;
@@ -10,10 +17,11 @@ window.addEventListener('DOMContentLoaded', function discoverDevices(evt) {
   var gBluetoothPowerStatus = document.querySelector('#bluetooth-status small');
   var gBluetoothDeviceLabel = document.querySelector('#bluetooth-connect small');
   var gBluetoothDiscoverLabel = document.querySelector('#bluetooth-discovery small');
-  var gBluetoothResultLabel = document.querySelector('#bluetooth-result span');
+  var gConnectedDeviceList = document.querySelector('#bluetooth-connected-devices');
   var gDeviceList = document.querySelector('#bluetooth-devices');
 
   var gDiscoveredDevices = new Array();
+  var gConnectedDevice = null;
 
   document.querySelector('#bluetooth-status input').onchange = function() {
     var req = gBluetoothAdapter.setEnabled(this.checked);
@@ -36,7 +44,7 @@ window.addEventListener('DOMContentLoaded', function discoverDevices(evt) {
   document.querySelector('#bluetooth-discovery input').onchange = function() {
     if (this.checked) {    
       gDiscoveredDevices.length = 0;
-      clearList();
+      clearList(gDeviceList);
 
       gBluetoothAdapter.startDiscovery();
     } else {
@@ -44,12 +52,23 @@ window.addEventListener('DOMContentLoaded', function discoverDevices(evt) {
     }
   };
 
+  gBluetoothAdapter.ondeviceconnected = function ondeviceconnected(evt) {
+    dump("Device connected: " + evt.device.name);
+    gConnectedDevice = evt.device;
+    gConnectedDeviceList.appendChild(newConnectedItem(evt.device.name));
+  }
+
+  gBluetoothAdapter.ondevicedisconnected = function ondevicedisconnected(evt) {
+    dump("Device disconnected" + evt.deviceAddress);
+    gConnectedDevice = null;
+    clearList(gConnectedDeviceList);
+  }
+
   gBluetoothAdapter.onpropertychanged = function onpropertychanged(evt) {
     dump("Get onpropertychanged");
     var propertyName = evt.propertyName;
 
-    if (propertyName == "Discovering")
-    {
+    if (propertyName == "Discovering") {
       if (gBluetoothAdapter.discovering) {
         gBluetoothDiscoverLabel.textContent = "Discovering";
       } else {
@@ -74,6 +93,27 @@ window.addEventListener('DOMContentLoaded', function discoverDevices(evt) {
     }
   };
 
+  function newConnectedItem(description) {
+    var a = document.createElement('a');
+    a.textContent = description;
+
+    var span = document.createElement('span');
+    span.className = 'bluetooth-connected';
+
+    var label = document.createElement('label');
+    label.appendChild(span);
+
+    var li = document.createElement('li');
+    li.appendChild(a);
+    li.appendChild(label);
+
+    li.onclick = function() {
+      gConnectedDevice.disconnect();
+    }
+
+    return li;
+  };
+
   function newScanItem(index, str) {
     var a = document.createElement('a');
     a.textContent = str;
@@ -90,31 +130,23 @@ window.addEventListener('DOMContentLoaded', function discoverDevices(evt) {
 
     li.onclick = function() {
       var device = gDiscoveredDevices[index];
+      var channel = device.queryServerChannel(uuidHandsfree);
 
-      if (device.connected) {
-        gBluetoothResultLabel.textContent = "Disconnecting";
+      dump("Channel is " + channel);
 
-        if (device.disconnect()) {
-          gBluetoothResultLabel.textContent = "Disconnected";
-        } else {
-          gBluetoothResultLabel.textContent = "Disconnect Failed";
-        }
+      // -1 means not a HFP device
+      if (channel != -1) {        
+        device.connect(channel);
       } else {
-        gBluetoothResultLabel.textContent = "Connecting";
-
-        if (device.connect(2)) {
-          gBluetoothResultLabel.textContent = "Connected";
-        } else {
-          gBluetoothResultLabel.textContent = "Connect Failed";
-        }
+        dump("Not support HFP");
       }
     }
 
     return li;
   };
 
-  function clearList() {
-    while (gDeviceList.hasChildNodes())
-      gDeviceList.removeChild(gDeviceList.lastChild);
+  function clearList(list) {
+    while (list.hasChildNodes())
+      list.removeChild(list.lastChild);
   };
 });
